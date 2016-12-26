702064abbbce8e356959dfb83e563e79d448163f
Lawrence Chen <lawrencechen14@gmail.com>
2016-12-26 01:30:55 -0800

    Update

---
+++ AssetManager.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class AssetManager {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton startButton = new JButton("Continue");
+        ActionListener startButtonListener = new StartButtonListener();
+        startButton.addActionListener(startButtonListener);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(startButton);
+        frame.setSize(1000, 1000);
+        frame.setVisible(true);
+    }
+
+    class StartButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("I was pressed!");
+        }
+    }
+
+}
