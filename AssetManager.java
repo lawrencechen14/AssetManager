c0bea1acd60704c8a8b840067a0f7be869993dcb
Lawrence Chen <lawrencechen14@gmail.com>
2016-12-28 01:50:02 -0800

    Update

--- AssetManager.java
+++ AssetManager.java
@@ -2,34 +2,65 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class AssetManager {
+public class AssetManager implements ActionListener {
+    static AssetManager gui = new AssetManager();
+    static JFrame frame = new JFrame();
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JButton startButton = new JButton("Continue");
-        JButton nextButton = new JButton("Next");
-        ActionListener startButtonListener = new StartButtonListener();
-        startButton.addActionListener(startButtonListener);
-        ActionListener nextButtonListener = new NextButtonListener();
-        startButton.addActionListener(nextButtonListener);
-
+        frame.add(Start());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(startButton);
-        frame.getContentPane().add(nextButton);
-        frame.setSize(1000, 1000);
+        frame.setSize(3000, 2000);
         frame.setVisible(true);
     }
 
-    public static class StartButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("I was pressed!");
-        }
+    public static JPanel Start() {
+        JPanel start = new JPanel(new BorderLayout());
+
+        JButton startButton = new JButton("Start");
+        startButton.setFont(new Font("Arial", Font.PLAIN, 40));
+        startButton.setPreferredSize(new Dimension(300,100));
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                frame.getContentPane().removeAll();
+                frame.getContentPane().add(Menu());
+                frame.revalidate();
+            }
+        });
+
+        JButton aboutButton = new JButton("About");
+        aboutButton.setFont(new Font("Arial", Font.PLAIN, 40));
+        aboutButton.setPreferredSize(new Dimension(300,100));
+        aboutButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                frame.getContentPane().removeAll();
+                frame.getContentPane().add(Menu());
+                frame.revalidate();
+            }
+        });
+
+        start.add(startButton, BorderLayout.CENTER);
+        start.add(aboutButton, BorderLayout.CENTER);
+
+        return start;
     }
 
-    public static class NextButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("I was pressed too!");
-        }
+    public static JPanel Menu() {
+        JPanel menu = new JPanel();
+        FlowLayout flow = new FlowLayout();
+        menu.setLayout(flow);
+
+        menu.add(new JButton("Button 1"));
+        menu.add(new JButton("Button 2"));
+        menu.add(new JButton("Button 3"));
+        menu.add(new JButton("Button 4"));
+        menu.add(new JButton("Button 5"));
+
+        return menu;
     }
 
+    public void actionPerformed(ActionEvent event) {
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(Menu());
+        frame.revalidate();
+    }
 }
